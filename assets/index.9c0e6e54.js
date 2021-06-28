432adb0235561890cbca618595129c9640d2e7e5
Mei <74983120+meixsu@users.noreply.github.com>
2021-06-28 22:17:10 +0800

    updated files

--- assets/index.9c0e6e54.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as o,a as r,b as s}from"./vendor.8d22552f.js";const a={},e={class:"container prose"},t=r("h1",null,"Mei Su",-1);a.render=function(r,s){return n(),o("div",e,[t])};s(a).mount("#app");
